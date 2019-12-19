da0c7eb6c71f33b56547e42d7029ebeabca28f69
Alex Kravchuk <akravchuk89@gmail.com>
2019-12-19 14:28:15 +0200

    init: added lab1, lab2

---
+++ lab1/main.cpp
@@ -0,0 +1,23 @@
+﻿/*
+2. Коло вписане в квадрат із заданою площею. Знайти площу квадрата, вписаного у це коло. Визначити у скільки разів площа вписаного квадрата менша за площу заданого квадрата.
+*/
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	const int cn = 2;
+	double s1, s2;
+	s1 = s2 = 0;
+
+	cout << "s1="; cin >> s1;
+	s2 = s1 / cn;
+
+	printf("s2 square is %d times bigger than s1 square\n", cn);
+	printf("s2=%.2f\n", s2);
+
+	cin.get();
+	return 0;
+}
---
+++ lab2/main.cpp
@@ -0,0 +1,56 @@
+﻿/*
+2. Заданий рядок символів, що містить цифри і пробіли. Групи цифр, що розділені пробілами (одним або декількома), вважаємо словами. Розглядаючи кожне слово як число, визначити їх суму.
+*/
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+char SPACE = ' ';
+
+char str[100];
+int num[100];
+
+void create_numbers(char[], int[]);
+void get_sum(int[]);
+
+int main()
+{
+	puts("enter numbers separated with space(s):");
+	gets_s(str);
+
+	create_numbers(str, num);
+	get_sum(num);
+
+	cin.get();
+	return 0;
+}
+
+void create_numbers(char str[], int num[])
+{
+	int k = 0;
+	char* delimeter = &SPACE;
+	char* p;
+	char* np = NULL;
+
+	p = strtok_s(str, delimeter, &np);
+	while (p != NULL)
+	{
+		// convert string to int
+		int current = _strtoi64(p, NULL, 10);
+		num[k] = current;
+		k++;
+		p = strtok_s(NULL, delimeter, &np);
+	}
+}
+
+void get_sum(int num1[])
+{
+	int sum = 0;
+
+	for (int i = 0; i < sizeof num1; i++)
+	{
+		sum += num1[i];
+	}
+	cout << "sum=" << sum << endl;
+}
